ad17f09fc499ce879ac0c8b9cfc4431c89431e98
JYH <joung5846@naver.com>
2022-03-24 23:28:08 +0900

    22.03.24

---
+++ C#basicforUnity/Basic/01_출력/01_출력.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic._01_출력
+{
+    class _01_출력
+    {
+        static void Main()
+        {
+            // using - 사용중
+            // namespace - 이름 공간 (특정한 이름으로 여러 기능들을 묶을 수 있는 기능)
+
+            // 클래스는 기본적으로 데이터와 메서드(기능)으로 이루어졌습니다.
+            // C# 프로그램을 구성하는 기본 단위 -> 클래스
+
+            // Main : 프로그램 시작점 (엔트리 포인트)
+            // 프로그램에 하나만 존재 해야함
+
+            // 콘솔에 출력
+            // Console.Write, Console.WriteLine 사용
+
+            // Write :  개행 없이 문자열 출력
+
+            Console.Write("Hello World\n");
+            // \n : 개행문자
+            // ; : 해당 라인이 끝났다는 것을 의미하는 문법적 요소
+
+            // WriteLine : 문자열 출력 후 한 줄 개행
+            Console.WriteLine("안녕");
+
+            // 코드를 프로그램으로 변환하는 작업 -> 빌드(Build)
+            // 빌드 단축키 : Ctrl + F5
+        }
+    }
+}
---
+++ C#basicforUnity/C#basicforUnity/Basic/02_변수/01_기본데이터형식/02_기본데이터형식.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic._02_변수._01_기본데이터형식
+{
+    class _02_기본데이터형식
+    {
+        static void Main(string[] args)
+        {
+            // 2진수, 16진수를 표현하는 방식
+            // 각 자리수의 가중치를 곱하는 방식으로 모두 계산할수 있습니다.
+            // 가중치는 x^n을 뜻합니다.
+            // 2진수형식은 0b 접두어를, 16진수 0x 접두어를 사용
+
+            // 2진수 데이터를 표현할 때, 각 자리를 확인하기 편하게
+            // 4자리마다 _(언더바)로 나눠서 표현
+            int number = 0b1111_0000;
+            Console.WriteLine(number);//240
+
+            int number2 = 0b1011_1010;
+            Console.WriteLine(number2);//186
+
+            // 16진수 0~9, A~F(10~15)
+            int number3 = 0xf0;
+            Console.WriteLine(number3);//240
+
+            int number4 = 0xba;
+            Console.WriteLine(number4);//186
+        }
+    }
+}
